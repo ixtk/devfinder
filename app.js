6f4f8e42f495f2fd130dcf26ee8a4fc991f822a4
ixtk <ikhcamot@gmail.com>
2022-03-29 22:29:59 +0400

    Minor improvements

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 let userInfo;
 
-document.querySelector('.search-btn').onclick = e => {
+document.querySelector('form').onsubmit = e => {
+  // TODO validation
+  e.preventDefault()
   const searchInput = document.querySelector('.search-input')
   getUserInfo(searchInput.value)
 }
 
 const fillData = json => {
+  // TODO textContent vs other attrs
   document.querySelector('.user-avatar').src = json.avatar_url
   document.querySelector('.username').textContent = json.login
   document.querySelector('.user-link').innerHTML = `<a href=${json.html_url}>@${json.login}</a>`
-  document.querySelector('time').textContent = new Date(json.created_at).toLocaleDateString('en-US')
+  // TODO format date
+  const joinTime = new Date(json.created_at).toLocaleDateString('en-US')
+  document.querySelector('.join-time').innerHTML = `Joined at <time datetime="${joinTime}">${joinTime}</time>`
 
-  document.querySelector('.user-bio').textContent = json.bio || 'Not available'
+  document.querySelector('.user-bio').textContent = json.bio || 'No bio'
 
   document.querySelector('.followers').textContent = json.followers
   document.querySelector('.following').textContent = json.following
@@ -26,9 +31,17 @@ const fillData = json => {
 const getUserInfo = async username => {
   try {
     const response = await fetch(`https://api.github.com/users/${username}`)
+    if (response.status === 404) {
+      console.log('Not found')
+      return
+    }
     jsonResponse = await response.json()
+
+    // TODO handle errors
     fillData(jsonResponse)
   } catch (err) {
     console.error(err)
   }
 }
+
+getUserInfo('octocat')
