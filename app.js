0d8cd52800281695a3f80421566364662e72f875
ixtk <ikhcamot@gmail.com>
2022-04-04 15:24:53 +0400

    General improvements

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 let userInfo;
 
+const searchBtn = document.querySelector('.search-btn')
+const searchInput = document.querySelector('.search-input')
+
+searchInput.onkeyup = e => {
+  if (searchBtn.disabled && searchInput.value !== '')
+    searchBtn.disabled = false
+  else if (searchInput.value === '')
+    searchBtn.disabled = true
+}
+
 document.querySelector('form').onsubmit = e => {
-  // TODO validation
   e.preventDefault()
-  const searchInput = document.querySelector('.search-input')
+  if (searchInput.value === '') return
   getUserInfo(searchInput.value)
 }
 
 const fillData = json => {
-  // TODO textContent vs other attrs
+  const joinTime = new Date(json.created_at).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric'
+  })
+  document.querySelector('.join-time').innerHTML = `Joined <time>${joinTime}</time>`
+
+  const githubLink = document.querySelector('.user-link')
+  githubLink.href = json.html_url
+  githubLink.textContent = `@${json.login}`
+
   document.querySelector('.user-avatar').src = json.avatar_url
   document.querySelector('.username').textContent = json.login
-  document.querySelector('.user-link').innerHTML = `<a href=${json.html_url}>@${json.login}</a>`
-  // TODO format date
-  const joinTime = new Date(json.created_at).toLocaleDateString('en-US')
-  document.querySelector('.join-time').innerHTML = `Joined at <time datetime="${joinTime}">${joinTime}</time>`
-
   document.querySelector('.user-bio').textContent = json.bio || 'No bio'
 
   document.querySelector('.followers').textContent = json.followers
@@ -25,19 +39,17 @@ const fillData = json => {
   document.querySelector('.company').textContent = json.company || 'No company'
   document.querySelector('.twitter').textContent = json.twitter_username || 'Not available'
   document.querySelector('.location').textContent = json.location || 'Not available'
-  document.querySelector('.blog').innerHTML = json.blog ? `<a href=${json.blog}>${json.blog}</a>` : 'No blog'
+  document.querySelector('.website').innerHTML = json.blog ? `<a href=${json.blog}>Website</a>` : 'No website'
 }
 
 const getUserInfo = async username => {
   try {
     const response = await fetch(`https://api.github.com/users/${username}`)
     if (response.status === 404) {
-      console.log('Not found')
+      console.log('User not found')
       return
     }
     jsonResponse = await response.json()
-
-    // TODO handle errors
     fillData(jsonResponse)
   } catch (err) {
     console.error(err)
